a5759ee1534b4e6a7aa13cd2b5c5380c3c86f1c6
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-07-08 19:24:32 +0100

    Added new routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('names/create', 'NameController@create');
+Route::post('names', 'NameController@store');
