8b30b7dbdb3c72a0ead68afb6ec55486588d36a6
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-07-21 19:13:15 +0100

    Updated view all the names

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,255 +1,86 @@
-<html>
- <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Laravel 5.8 - DataTables Server Side Processing using Ajax</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-  <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>  
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
- </head>
- <body>
-  <div class="container">    
-     <br />
-     <h3 align="center">Laravel 5.8 Ajax Crud Tutorial - Delete or Remove Data</h3>
-     <br />
-     <div align="right">
-      <button type="button" name="create_record" id="create_record" class="btn btn-success btn-sm">Create Record</button>
-     </div>
-     <br />
-   <div class="table-responsive">
-    <table class="table table-bordered table-striped" id="user_table">
-           <thead>
-            <tr>
-                <th width="10%">Image</th>
-                <th width="35%">First Name</th>
-                <th width="35%">Last Name</th>
-                <th width="30%">Action</th>
-            </tr>
-           </thead>
-       </table>
-   </div>
-   <br />
-   <br />
-  </div>
- </body>
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Random Winners</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/master.css') }}">
+    </head>
+    <body>
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<ul class="nav navbar-nav">
+				<li>Select Random Winners</li>
+			</ul>
+		</nav>
+	    <br><br><br><br>
+     	<div class="container">
+    		<div class="form-group row add">
+    			<div class="col-md-8">
+    				<input type="text" class="form-control" id="name" name="name"
+    					placeholder="Enter some name" required>
+    				<p class="error text-center alert alert-danger hidden"></p>
+    			</div>
+    			<div class="col-md-4">
+    				<button class="btn btn-primary" type="submit" id="add">
+    					<span class="glyphicon glyphicon-plus"></span>
+    				</button>
+    			</div>
+    		</div>
+			 {{ csrf_field() }}
+			 <div class="container withBorder">
+				<div class="col-md-3">
+					<div class="table-responsive text-center">
+						<table class="table table-borderless" id="table">
+							@foreach($names as $name)
+							<tr class="name{{$name->id}}">
+								<!-- <td>{{$name->id}}</td> -->
+								<td>{{$name->first_name}}</td>
+								<td>
+									<button class="delete-modal btn btn-danger"
+										data-id="{{$name->id}}" data-name="{{$name->first_name}}">
+										<span class="glyphicon glyphicon-minus"></span>
+									</button>
+								</td>
+							</tr>
+							@endforeach
+						</table>
+					</div>
+				</div>				
+			</div>
+  		</div>
+  		<div id="myModal" class="modal fade" role="dialog">
+  			<div class="modal-dialog">
+  			<!-- Modal content-->
+  				<div class="modal-content">
+  					<div class="modal-header">
+  						<button type="button" class="close" data-dismiss="modal">&times;</button>
+  						<h4 class="modal-title"></h4>
+  					</div>
+  					<div class="modal-body">
+  						<div class="deleteContent">
+  							Are you Sure you want to delete <strong><span class="dname"></span></strong>? <span
+  								class="hidden did"></span>
+  						</div>
+  						<div class="modal-footer">
+  							<button type="button" class="btn actionBtn" data-dismiss="modal">
+  								<span id="footer_action_button" class='glyphicon'> </span>
+  							</button>
+  							<button type="button" class="btn btn-warning" data-dismiss="modal">
+  								<span class='glyphicon glyphicon-remove'></span> Close
+  							</button>
+  						</div>
+  					</div>
+  				</div>
+		  	</div>
+		</div>
+      <script src="{{ asset('js/app.js') }}"></script>
+    </body>
 </html>
-
-<div id="formModal" class="modal fade" role="dialog">
- <div class="modal-dialog">
-  <div class="modal-content">
-   <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Add New Record</h4>
-        </div>
-        <div class="modal-body">
-         <span id="form_result"></span>
-         <form method="post" id="sample_form" class="form-horizontal" enctype="multipart/form-data">
-          @csrf
-          <div class="form-group">
-            <label class="control-label col-md-4" >First Name : </label>
-            <div class="col-md-8">
-             <input type="text" name="first_name" id="first_name" class="form-control" />
-            </div>
-           </div>
-           <div class="form-group">
-            <label class="control-label col-md-4">Last Name : </label>
-            <div class="col-md-8">
-             <input type="text" name="last_name" id="last_name" class="form-control" />
-            </div>
-           </div>
-           <div class="form-group">
-            <label class="control-label col-md-4">Select Profile Image : </label>
-            <div class="col-md-8">
-             <input type="file" name="image" id="image" />
-             <span id="store_image"></span>
-            </div>
-           </div>
-           <br />
-           <div class="form-group" align="center">
-            <input type="hidden" name="action" id="action" />
-            <input type="hidden" name="hidden_id" id="hidden_id" />
-            <input type="submit" name="action_button" id="action_button" class="btn btn-warning" value="Add" />
-           </div>
-         </form>
-        </div>
-     </div>
-    </div>
-</div>
-
-<div id="confirmModal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h2 class="modal-title">Confirmation</h2>
-            </div>
-            <div class="modal-body">
-                <h4 align="center" style="margin:0;">Are you sure you want to remove this data?</h4>
-            </div>
-            <div class="modal-footer">
-             <button type="button" name="ok_button" id="ok_button" class="btn btn-danger">OK</button>
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<script>
-$(document).ready(function(){
-
- $('#user_table').DataTable({
-  processing: true,
-  serverSide: true,
-  ajax:{
-   url: "{{ route('welcome') }}",
-  },
-  columns:[
-   {
-    data: 'image',
-    name: 'image',
-    render: function(data, type, full, meta){
-     return "<img src={{ URL::to('/') }}/images/" + data + " width='70' class='img-thumbnail' />";
-    },
-    orderable: false
-   },
-   {
-    data: 'first_name',
-    name: 'first_name'
-   },
-   {
-    data: 'last_name',
-    name: 'last_name'
-   },
-   {
-    data: 'action',
-    name: 'action',
-    orderable: false
-   }
-  ]
- });
-
- $('#create_record').click(function(){
-  $('.modal-title').text("Add New Record");
-     $('#action_button').val("Add");
-     $('#action').val("Add");
-     $('#formModal').modal('show');
- });
-
- $('#sample_form').on('submit', function(event){
-  event.preventDefault();
-  if($('#action').val() == 'Add')
-  {
-   $.ajax({
-    url:"{{ route('ajax-crud.store') }}",
-    method:"POST",
-    data: new FormData(this),
-    contentType: false,
-    cache:false,
-    processData: false,
-    dataType:"json",
-    success:function(data)
-    {
-     var html = '';
-     if(data.errors)
-     {
-      html = '<div class="alert alert-danger">';
-      for(var count = 0; count < data.errors.length; count++)
-      {
-       html += '<p>' + data.errors[count] + '</p>';
-      }
-      html += '</div>';
-     }
-     if(data.success)
-     {
-      html = '<div class="alert alert-success">' + data.success + '</div>';
-      $('#sample_form')[0].reset();
-      $('#user_table').DataTable().ajax.reload();
-     }
-     $('#form_result').html(html);
-    }
-   })
-  }
-
-  if($('#action').val() == "Edit")
-  {
-   $.ajax({
-    url:"{{ route('ajax-crud.update') }}",
-    method:"POST",
-    data:new FormData(this),
-    contentType: false,
-    cache: false,
-    processData: false,
-    dataType:"json",
-    success:function(data)
-    {
-     var html = '';
-     if(data.errors)
-     {
-      html = '<div class="alert alert-danger">';
-      for(var count = 0; count < data.errors.length; count++)
-      {
-       html += '<p>' + data.errors[count] + '</p>';
-      }
-      html += '</div>';
-     }
-     if(data.success)
-     {
-      html = '<div class="alert alert-success">' + data.success + '</div>';
-      $('#sample_form')[0].reset();
-      $('#store_image').html('');
-      $('#user_table').DataTable().ajax.reload();
-     }
-     $('#form_result').html(html);
-    }
-   });
-  }
- });
-
- $(document).on('click', '.edit', function(){
-  var id = $(this).attr('id');
-  $('#form_result').html('');
-  $.ajax({
-   url:"/ajax-crud/"+id+"/edit",
-   dataType:"json",
-   success:function(html){
-    $('#first_name').val(html.data.first_name);
-    $('#last_name').val(html.data.last_name);
-    $('#store_image').html("<img src={{ URL::to('/') }}/images/" + html.data.image + " width='70' class='img-thumbnail' />");
-    $('#store_image').append("<input type='hidden' name='hidden_image' value='"+html.data.image+"' />");
-    $('#hidden_id').val(html.data.id);
-    $('.modal-title').text("Edit New Record");
-    $('#action_button').val("Edit");
-    $('#action').val("Edit");
-    $('#formModal').modal('show');
-   }
-  })
- });
-
- var user_id;
-
- $(document).on('click', '.delete', function(){
-  user_id = $(this).attr('id');
-  $('#confirmModal').modal('show');
- });
-
- $('#ok_button').click(function(){
-  $.ajax({
-   url:"ajax-crud/destroy/"+user_id,
-   beforeSend:function(){
-    $('#ok_button').text('Deleting...');
-   },
-   success:function(data)
-   {
-    setTimeout(function(){
-     $('#confirmModal').modal('hide');
-     $('#user_table').DataTable().ajax.reload();
-    }, 2000);
-   }
-  })
- });
-
-});
-</script>
